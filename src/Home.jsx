0c3d83afb97c65f5712f9dd123d5c1420ef32171
inn-creat <ugonnaachilaonu@gmail.com>
2026-03-13 18:31:23 +0100

    fix: correct all app.css import casing

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './app.css';
+import './App.css';
 import Navbar from './navbar';
 import genphoto from './assets/genphoto.png'; 
 import coffee from './assets/coffee.png';
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './app.css';
+import './App.css';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
